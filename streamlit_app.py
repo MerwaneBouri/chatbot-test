a138430106b17e017bf756d5b3f66d962dfade42
Merwane Bouri <115326187+MerwaneBouri@users.noreply.github.com>
2025-03-11 06:43:25 +0000

    updated title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+# met moi l'emoji food
+st.title("🍔 mIAm")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
